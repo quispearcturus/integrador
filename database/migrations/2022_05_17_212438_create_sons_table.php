bb8a0cd6ec42fc52be3be88c9e82ed3f8a8c0c5c
quispearcturus <48285622+quispearcturus@users.noreply.github.com>
2022-05-17 19:38:55 -0500

    Add files via upload

---
+++ database/migrations/2022_05_17_212438_create_sons_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sons', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('celphone');
+            $table->string('dni');
+            $table->string('email');
+            $table->number_format('age');
+
+            $table->unsignedBigInteger('menber_id');
+            $table->foreign('menber_id')->references('id')->on('menbers')->onDelete('cascade');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sons');
+    }
+};
