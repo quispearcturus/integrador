8d08ce5b88f7d32dc9394dccd9fd3ea89996b192
quispearcturus <48285622+quispearcturus@users.noreply.github.com>
2022-05-17 19:36:17 -0500

    Add files via upload

---
+++ app/Models/sons.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class sons extends Model
+{
+    use HasFactory;
+}
